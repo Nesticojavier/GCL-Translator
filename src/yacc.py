5b38dfe254b756b6bce12670b2a140b0a6f8d1dc
Nestor Javier <Nestico@Nestors-MacBook-Pro.local>
2022-11-19 20:41:30 -0400

    listo el declare block

--- src/yacc.py
+++ src/yacc.py
@@ -1,5 +1,6 @@
 import ply.yacc as yacc
 from lexer import tokens
+from Utils.AST import *
 
 # precedence = (
 #     ('rigth', 'TkAsig')
@@ -13,14 +14,14 @@ def p_program(p):
     '''
     B : TkOBlock DECLARE INSTRUCTIONS TkCBlock
     '''
-    p[0] = p[2]
+    p[0] = Nodo("Block", p[2], p[3])
     print(f'******** Todo bien **********')
 
 def p_declare(p):
     '''
     DECLARE : TkDeclare LIST_DECLARE
     '''
-    p[0] = (p[1],p[2])
+    p[0] = Nodo("Declare", p[2])
 
 def p_list_declare_base(p):
     '''
@@ -30,21 +31,22 @@ def p_list_declare_base(p):
 
 def p_list_declare(p):
     '''
-    LIST_DECLARE : VARIABLE_DECLARATION TkSemicolon LIST_DECLARE
+    LIST_DECLARE : LIST_DECLARE TkSemicolon VARIABLE_DECLARATION
     '''
-    p[0] = ('Sequencing',p[1], p[3])
+    p[0] = Nodo('Sequencing', p[1], p[3])
 
                 
 def p_list_variables_declare_base(p):
     '''
     VARIABLE_DECLARATION : TkId TkTwoPoints TYPE
     '''
-    p[0] = ('declaracion', f"{p[1]} {p[2]} {p[3]}")
+    p[0] = Nodo(f"{p[1]} {p[2]} {p[3]}")
 
 def p_list_variables_declare(p):
     '''
     VARIABLE_DECLARATION : TkId TkComma VARIABLE_DECLARATION
     '''
+    p[0] = Nodo(f"{p[1]}{p[2]} " + f"{p[3]}")
 
 def p_type_varible_declare(p):
     '''
@@ -60,6 +62,7 @@ def p_statements(p):
     INSTRUCTIONS : INSTRUCTION TkSemicolon INSTRUCTIONS
                   | INSTRUCTION
     '''
+    p[0] = Nodo("Hola")
 
 def p_statement(p):
     '''
@@ -70,15 +73,15 @@ def p_statement(p):
                 | TkId
     '''
 
-parser = yacc.yacc()
+parser = yacc.yacc('SLR')
 
 data = '''
 |[ 
     declare
         h : int;
-        g : array;
-        g, a, b, c, d, e : int
-        //y : bool
+        g, h, k : array;
+        f : int;
+        y, x: bool
 
     print;
     do;
@@ -89,5 +92,9 @@ data = '''
 '''
 
 ast = parser.parse(data)
-print(ast)
+# print(ast)
+
+printArbol(ast)
+
 # print(result)
+
